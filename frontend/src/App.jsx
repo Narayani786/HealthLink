62406de3cf210fdeaaeaa1057515cff14fa2d91b
Saloni <saloninarayana1@gmail.com>
2025-08-11 19:38:42 +0530

    main logic of doctors

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar.jsx';
+import Footer from './components/Footer.jsx';
+import Home from './pages/Home.jsx';
+import DoctorList from './pages/DoctorList.jsx';
+import DoctorDetails from './pages/DoctorDetails.jsx';
+
+export default function App() {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path='/'element={<Home />} />
+                <Route path='/doctors'element={<DoctorList />}/>
+                <Route path='/doctors/:id'element={<DoctorDetails />}/>
+            </Routes>
+            <Footer />
+        </Router>
+    )
+};
