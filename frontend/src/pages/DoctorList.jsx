60978dbb09e1db539b5b6379c20a3e65e2fc1091
Saloni <saloninarayana1@gmail.com>
2025-08-11 20:33:54 +0530

    added searchbar logic and handleSearch from searchbar file

--- frontend/src/pages/DoctorList.jsx
+++ frontend/src/pages/DoctorList.jsx
@@ -1,19 +1,33 @@
 import { useEffect, useState } from "react";
 import { getDoctors } from '../services/doctorServices.js';
 import DoctorCard from '../components/DoctorCard.jsx';
+import SearchBar from "../components/SearchBar.jsx";
 
 export default function DoctorList() {
-    const [doctor, setDoctor] = useState([]);
+    const [doctors, setDoctors] = useState([]);
+    const [filteredDoctors, setFilteredDoctors] = useState([]);
 
     useEffect(() => {
-        getDoctors().then(setDoctor);
+        getDoctors().then((data) => {
+            setDoctors(data);
+            setFilteredDoctors(data);
+        });
     }, []);
+
+    const handleSearch = (query) => {
+        const filtered = doctors.filter((doc) =>
+        doc.name.toLowerCase().includes(query.toLowerCase()) ||
+        doc.specialization.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredDoctors(filtered);
+    };
 
     return (
         <div style={{ padding: "20px" }}>
             <h2>Doctors List</h2>
-            <div style={{ display: 'flex', flexWeap: 'wrap' }}>
-                {doctor.map((doc) => (
+            <SearchBar onSearch={handleSearch} />
+            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                {filteredDoctors.map((doc) => (
                     <DoctorCard key={doc.id} doctor={doc} />
                 ))}
             </div>
